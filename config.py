f20f53ce24fd4b9cfdf0dd9271f349e5561d237c
Danbesy <136319080+Danbesy@users.noreply.github.com>
2025-02-28 22:54:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TOKEN = "BotFatherToken" # API токен бота - @BotFather
-CRYPTOPAY_TOKEN = "CryptoPayToken" # API токен CryptoPay - @CryptoBot
+TOKEN = "Token BotFather" # API токен бота - @BotFather
+CRYPTOPAY_TOKEN = "Token CryptoPay" # API токен CryptoPay - @CryptoBot
 ADMINS = [5501024918] # Администраторы бота, айди можно получить через - @getmyid_bot
