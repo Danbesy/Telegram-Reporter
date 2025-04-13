19110fbd94aef469391b6e385fbb7deb92bb6978
Danbesy <136319080+Danbesy@users.noreply.github.com>
2025-04-13 15:57:59 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+TOKEN = "BotFatherToken" # API токен бота - @BotFather
+CRYPTOPAY_TOKEN = "CryptoPayToken" # API токен CryptoPay - @CryptoBot
+ADMINS = [5501024918] # Администраторы бота, айди можно получить через - @getmyid_bot
---
+++ main.py
@@ -0,0 +1,32 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.types import BotCommand
+from handlers import router
+from config import *
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+# Основная функция
+async def main():
+    dp.include_router(router)
+
+    # Удаляем старые команды    
+    await bot.delete_my_commands()
+
+    commands = [
+        BotCommand(command="/start", description="Перезапустить бота 🔁 Restart bot"),
+        BotCommand(command="/language", description="Смена языка 🌐 Change language")
+    ]
+    await bot.set_my_commands(commands)
+
+    # Запускаем поллинг
+    await dp.start_polling(bot)
+
+# Запуск программы
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')
+        
